9923adb7186b34e80a1af6518b7e5f774e78ce6a
seriesli <1211198003@qq.com>
2024-10-08 22:45:47 +0800

    各个模块 build.gradle 的抽离

--- 开源框架/组件化框架设计/代码/ComponentsDesign/app/src/main/java/cn/series/componentsdesign/MainActivity.kt
+++ 开源框架/组件化框架设计/代码/ComponentsDesign/app/src/main/java/cn/series/componentsdesign/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val baseTest =  BaseTest()
 //        BuildConfig
-        Log.d("lmx", BuildConfig.VERSION_NAME)
+        Log.d("lmx", BuildConfig.xxx.toString())
 
     }
 }
